a74e0405ac131218366a12b75ada74ba2ba7cf33
Skaistev <153711410+Skaistev@users.noreply.github.com>
2024-03-01 12:17:08 +0200

    script

--- counter.js
+++ counter.js
@@ -1,18 +1,34 @@
-const weightDOM = document.getElementById("input-value1")
-const heightDOM = document.getElementById("input-value2")
+const weightDOM = document.getElementById("input-value2")
+const heightDOM = document.getElementById("input-value1")
 const btnCountDOM = document.getElementById("count")
 const answerRowDOM = document.querySelector(".answer-row")
+const answerDOM = document.querySelector(".answer")
 
-console.log(answerRowDOM);
+console.log(answerDOM);
 
 
 function kmiCount (){
 
-const x = weightDOM.value*heightDOM.value;
+const kmi = ((weightDOM.value/((heightDOM.value)/100)))/(heightDOM.value/100);
+
+let ats= ""
+
+if(kmi<=18.5){
+   ats = "Your weigth is poor. Go eat something!"
+  
+}else
+if(kmi>18.5<=24.9){
+    ats = "Your weigth is perfect. Keep going!"
+} else
+if(kmi>=25<=29.9){
+    ats = "Your weigth is rather big... Go do some sport activities and eat more vegetables!"
+} else
+if(kmi>=30){
+    ats = "Don't panic! Your weight is too big for human being. Try change your habits and hit into healhier way of living."
+}
 
 
-
-answerRowDOM.innerHTML = `<p class="answer">Your KMI is ${x}.</p>
+return answerRowDOM.innerHTML = `<p class="answer">Your KMI is ${kmi.toFixed(2)}. ${ats}</p>
 <button id="clear" class="btn">Clear</button>`
 
 
