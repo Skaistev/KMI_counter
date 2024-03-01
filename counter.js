cc4b7142e47fe359749aaa26ef92305ddb242f4f
Skaistev <153711410+Skaistev@users.noreply.github.com>
2024-03-01 13:04:37 +0200

    papil

--- counter.js
+++ counter.js
@@ -3,35 +3,46 @@ const heightDOM = document.getElementById("input-value1")
 const btnCountDOM = document.getElementById("count")
 const answerRowDOM = document.querySelector(".answer-row")
 const answerDOM = document.querySelector(".answer")
+const btnClearDOM = document.getElementById("clear")
 
-console.log(answerDOM);
+console.log(btnClearDOM);
 
 
 function kmiCount (){
 
 const kmi = ((weightDOM.value/((heightDOM.value)/100)))/(heightDOM.value/100);
-
-let ats= ""
+let ats = ''
 
 if(kmi<=18.5){
-   ats = "Your weigth is poor. Go eat something!"
-  
-}else
-if(kmi>18.5<=24.9){
+
+  ats = "Your weigth is poor. Go eat something!"
+  answerRowDOM.classList.add('yellow');
+
+}else 
+if (kmi>18.5 && kmi<=24.9){
     ats = "Your weigth is perfect. Keep going!"
-} else
-if(kmi>=25<=29.9){
+    answerRowDOM.classList.add('green');
+
+}else 
+if(kmi>=25&&kmi<29.9){
     ats = "Your weigth is rather big... Go do some sport activities and eat more vegetables!"
-} else
-if(kmi>=30){
+    answerRowDOM.classList.add('orange');
+
+}else
+if (kmi>=30){
     ats = "Don't panic! Your weight is too big for human being. Try change your habits and hit into healhier way of living."
+    answerRowDOM.classList.add('red');
 }
-
-
 return answerRowDOM.innerHTML = `<p class="answer">Your KMI is ${kmi.toFixed(2)}. ${ats}</p>
 <button id="clear" class="btn">Clear</button>`
 
-
+}
+  
+function clearButton () {
+    
+    weightDOM.value = 0;
+    heightDOM.value = 0;
 }
 
-btnCountDOM.addEventListener("click", kmiCount)
+btnCountDOM.addEventListener("click", kmiCount);
+btnClearDOM.addEventListener("click", clearButton);
